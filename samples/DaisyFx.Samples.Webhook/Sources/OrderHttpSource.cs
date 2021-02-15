34601aa9dbc484af2d4994b2aae4f2634b27dec4
Patrik Lundqvist <pat.lundqvist@gmail.com>
2021-02-15 17:26:41 +0100

    Remove unused usings (#5)

--- samples/DaisyFx.Samples.Webhook/Sources/OrderHttpSource.cs
+++ samples/DaisyFx.Samples.Webhook/Sources/OrderHttpSource.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using System.Threading.Tasks;
 using DaisyFx.Samples.Webhook.Model;
 using DaisyFx.Sources.Http;
--- src/DaisyFx.Raygun/RaygunExceptionEventHandler.cs
+++ src/DaisyFx.Raygun/RaygunExceptionEventHandler.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Threading.Tasks;
 using DaisyFx.Events;
 using DaisyFx.Events.Chain;
